d8a079cf118d6ae325308bd468d4c5c087464388
Evânio Marques <evanio.economia@gmail.com>
2022-02-02 09:08:44 -0300

    Update webscraping.R

--- webscraping.R
+++ webscraping.R
@@ -21,8 +21,8 @@ lapply(list.of.packages, library, character.only = TRUE)
 # O termo "?o=" é o divisor de página, então, para realizar o scraping, o url_base 
 # fica com o link até o divisor, e complement o restante do link. Não se esqueça 
 # de remover o número referente à pagina. Por exemplo:
-url_base = 'https://sp.olx.com.br/sao-paulo-e-regiao/zona-oeste/imoveis/aluguel/2-quartos?gsp=1&o='
-complement = '&pe=2000&sd=2922&sd=2932&sd=2934&sd=2913&sd=2933&sd=2808&sd=2925&sd=2917&sd=2918&sd=2912&sd=2924&sd=2930&sd=2921&sd=2919&sd=2915&sd=2910&sd=2931&sd=2920&sd=2928&sd=2927&sd=2909&sd=2929'
+url_base = 'https://sp.olx.com.br/sao-paulo-e-regiao/zona-oeste/imoveis/aluguel?o='
+complement = '&pe=4000&sd=2922&sd=2932&sd=2915'
 url_num = read_html(paste0(url_base,1,complement))
 
 # Contando o número de páginas
@@ -53,76 +53,101 @@ for (j in 1:(num_pag)){
     cas = divs[[k]]
     cas = read_html(cas)
     
-    titulo = as.vector(cas %>% html_nodes('h1') %>% html_text())
-
-    preco = cas %>% html_nodes('h2') %>% html_text()
-    preco = preco[1]
-    
-    categoria = as.vector(cas %>% html_nodes('a') %>% html_text())
-    m = ifelse(is.na(as.numeric(categoria[14])),
-              ifelse(is.na(as.numeric(categoria[13])),1,-1),0)
-    
-    quartos = categoria[14+m]
-    
-    if (quartos=='') {
-      quartos = '1'
-      categoria = 'Não identificado'
-      condominio = 'R$ 0'
-      IPTU = 'R$ 0'
-      m2 = '0m²'
-      banheiros = '1'
-      vagas = '0'
-      Infos = cas %>% html_nodes('dd') %>% html_text()
-      bairro = ifelse(Infos[length(Infos)-1]==cidade,Infos[length(Infos)],Infos[length(Infos)-1])
-      datap = ifelse(as.vector(cas %>% html_nodes('span') %>% html_text())[11]=='Localização',
-                     as.vector(cas %>% html_nodes('span') %>% html_text())[12], 
-                     ifelse(as.vector(cas %>% html_nodes('span') %>% html_text())[11]=='-',
-                            as.vector(cas %>% html_nodes('span') %>% html_text())[10],
-                            as.vector(cas %>% html_nodes('span') %>% html_text())[11]))
-    } 
-    else {
+    Infos = cas %>% html_nodes('dd') %>% html_text()
+    if(Infos[length(Infos)-2]==cidade | Infos[length(Infos)-1]==cidade | Infos[length(Infos)]==cidade){
       
-      if (is.na(as.numeric(quartos))) {
-        quartos = categoria[16]
-        categoria = categoria[14]
-      } 
-      else{
-        categoria = categoria[12+m]
-      }
+      titulo = as.vector(cas %>% html_nodes('h1') %>% html_text())
       
-      Infos = cas %>% html_nodes('dd') %>% html_text()
+      preco = cas %>% html_nodes('h2') %>% html_text()
+      preco = preco[1]
       
-      ref1 = substr(Infos[1], nchar(Infos[1])-2+1, nchar(Infos[1]))
-      ref2 = substr(Infos[2], nchar(Infos[2])-2+1, nchar(Infos[2]))
+      categoria = as.vector(cas %>% html_nodes('a') %>% html_text())
+      m = ifelse(is.na(as.numeric(categoria[14])),
+                 ifelse(is.na(as.numeric(categoria[13])),1,-1),0)
       
-      if (ref1=='m²') {
+      quartos = categoria[14+m]
+      
+      if (quartos=='') {
+        quartos = '1'
+        categoria = 'Não identificado'
         condominio = 'R$ 0'
         IPTU = 'R$ 0'
-        l = 2
-      } else if (ref2=='m²'){
-        condominio = Infos[1]
-        IPTU = 'R$ 0'
-        l = 1
-      } else {
-        condominio = Infos[1]
-        IPTU = Infos[2]
-        l = 0
+        m2 = '0m²'
+        banheiros = '1'
+        vagas = '0'
+        Infos = cas %>% html_nodes('dd') %>% html_text()
+        bairro = ifelse(Infos[length(Infos)-1]==cidade,Infos[length(Infos)],Infos[length(Infos)-1])
+        datap = ifelse(as.vector(cas %>% html_nodes('span') %>% html_text())[11]=='Localização',
+                       as.vector(cas %>% html_nodes('span') %>% html_text())[12], 
+                       ifelse(as.vector(cas %>% html_nodes('span') %>% html_text())[11]=='-',
+                              as.vector(cas %>% html_nodes('span') %>% html_text())[10],
+                              as.vector(cas %>% html_nodes('span') %>% html_text())[11]))
+      } 
+      else {
+        if (is.na(as.numeric(substr(quartos,1,1)))) {
+          quartos = categoria[16]
+          categoria = categoria[14]
+        } 
+        else{
+          categoria = categoria[12+m]
+        }
+        
+        Infos = cas %>% html_nodes('dd') %>% html_text()
+        
+        ref1 = substr(Infos[1], nchar(Infos[1])-2+1, nchar(Infos[1]))
+        ref2 = substr(Infos[2], nchar(Infos[2])-2+1, nchar(Infos[2]))
+        ref3 = substr(Infos[1], 1,1)
+        ref4 = substr(Infos[2], 1,1)
+        
+        if (ref1=='m²') {
+          condominio = 'R$ 0'
+          IPTU = 'R$ 0'
+          l = 2
+          r = 0
+        } 
+        else if (ref2=='m²'){
+          condominio = Infos[1]
+          IPTU = 'R$ 0'
+          l = 1
+          r = 0
+        } 
+        else if (ref3!='R'){
+          condominio = 'R$ 0'
+          IPTU = 'R$ 0'
+          r = 1
+          l = 2
+        }
+        else if (ref3=='R' & ref4!='R'){
+          condominio = Infos[1]
+          IPTU = 'R$ 0'
+          r = 1
+          l = 1
+        }
+        else {
+          condominio = Infos[1]
+          IPTU = Infos[2]
+          l = 0
+          r = 0
+        }
+        
+        m2 = ifelse(r==1,'0m²',Infos[3-l-r])
+        banheiros = Infos[4-l-r]
+        vagas = ifelse(nchar(Infos[5-l-r])==8,'0',Infos[5-l-r])
+        vagas = ifelse(is.na(as.numeric(vagas)),'0',vagas)
+        bairro = ifelse(Infos[length(Infos)-1]==cidade,Infos[length(Infos)],Infos[length(Infos)-1])
+        link = divs[[k]]
+        datap = ifelse(as.vector(cas %>% html_nodes('span') %>% html_text())[11]=='Localização',
+                       as.vector(cas %>% html_nodes('span') %>% html_text())[12], 
+                       ifelse(as.vector(cas %>% html_nodes('span') %>% html_text())[11]=='-',
+                              as.vector(cas %>% html_nodes('span') %>% html_text())[10],
+                              as.vector(cas %>% html_nodes('span') %>% html_text())[11]))
       }
       
-      m2 = Infos[3-l]
-      banheiros = Infos[4-l]
-      vagas = ifelse(nchar(Infos[5-l])==8,'0',Infos[5-l])
-      vagas = ifelse(is.na(as.numeric(vagas)),'0',vagas)
-      bairro = ifelse(Infos[length(Infos)-1]==cidade,Infos[length(Infos)],Infos[length(Infos)-1])
-      link = divs[[k]]
-      datap = ifelse(as.vector(cas %>% html_nodes('span') %>% html_text())[11]=='Localização',
-                     as.vector(cas %>% html_nodes('span') %>% html_text())[12], 
-                     ifelse(as.vector(cas %>% html_nodes('span') %>% html_text())[11]=='-',
-                            as.vector(cas %>% html_nodes('span') %>% html_text())[10],
-                            as.vector(cas %>% html_nodes('span') %>% html_text())[11]))
+      result = rbind(result, data.frame(titulo,datap,categoria,bairro,preco,condominio,IPTU,m2,quartos,banheiros,vagas,link))
+      print(paste('Anúncio',(k-11)))
     }
-    
-    result = rbind(result, data.frame(titulo,datap,categoria,bairro,preco,condominio,IPTU,m2,quartos,banheiros,vagas,link))
-    print(paste('Anúncio',(k-11)))
+    else{
+      print('Não corresponde à mesma cidade')
+    }
   }
 }
